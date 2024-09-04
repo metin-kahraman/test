d971ba18adf9fcc63c3d191333e94157d6815327
metin-kahraman <metinkahraman@outlook.com>
2024-09-04 19:48:03 +0300

    +6

--- src/app/theme/themePrimitives.ts
+++ src/app/theme/themePrimitives.ts
@@ -91,7 +91,7 @@ export const red = {
   900: 'hsl(0, 93%, 6%)',
 };
 
-export const getDesignTokens = (mode: PaletteMode) => ({
+const getDesignTokens = (mode: PaletteMode) => ({
   palette: {
     mode,
     primary: {
@@ -227,7 +227,12 @@ export const getDesignTokens = (mode: PaletteMode) => ({
   shadows: [
     'none',
     ...(mode === 'dark'
-      ? 'hsla(220, 30%, 5%, 0.7) 0px 4px 16px 0px, hsla(220, 25%, 10%, 0.8) 0px 8px 16px -5px'
-      : 'hsla(220, 30%, 5%, 0.07) 0px 4px 16px 0px, hsla(220, 25%, 10%, 0.07) 0px 8px 16px -5px'),
+      ? [
+          'hsla(220, 30%, 5%, 0.7) 0px 4px 16px 0px, hsla(220, 25%, 10%, 0.8) 0px 8px 16px -5px'
+        ]
+      : [
+          'hsla(220, 30%, 5%, 0.07) 0px 4px 16px 0px, hsla(220, 25%, 10%, 0.07) 0px 8px 16px -5px'
+        ]),
   ] as Shadows,
 });
+
